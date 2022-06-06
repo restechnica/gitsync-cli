23d9d40249bf7557166ea9d40df3618d426a2ac3
Olivier Van Aken <olivier.va@outlook.com>
2022-06-07 00:05:13 +0200

    added persistent verbose flag to root command - silenced errors on root command - added persistent prerun to root command which configures loglevel based on the verbose flag - added zerlog logger config to init() of root command

--- pkg/cli/commands/root.go
+++ pkg/cli/commands/root.go
@@ -1,20 +1,54 @@
 package commands
 
 import (
-	v1 "github.com/restechnica/gitsync-cli/pkg/cli/commands/v1"
+	"os"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
+
+	"github.com/restechnica/gitsync-cli/pkg/cli"
+	v1 "github.com/restechnica/gitsync-cli/pkg/cli/commands/v1"
 )
+
+func init() {
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+}
 
 // NewRootCommand creates a new root command.
 // Returns the new root command.
 func NewRootCommand() *cobra.Command {
 	var command = &cobra.Command{
-		Use: "gitsync",
+		PersistentPreRunE: RootCommandPersistentPreRunE,
+		SilenceErrors:     true,
+		Use:               "gitsync",
 	}
+
+	command.PersistentFlags().BoolVarP(&cli.VerboseFlag, "verbose", "v", false, "increase log level verbosity")
 
 	command.AddCommand(v1.NewV1Command())
 	command.AddCommand(v1.NewSyncCommand())
 	command.AddCommand(NewVersionCommand())
 
 	return command
+}
+
+// RootCommandPersistentPreRunE runs before the command and any subcommand runs.
+// Returns an error if it failed.
+func RootCommandPersistentPreRunE(cmd *cobra.Command, args []string) (err error) {
+	ConfigureLogging()
+
+	return err
+}
+
+func ConfigureLogging() {
+	SetLogLevel()
+}
+
+func SetLogLevel() {
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+
+	if cli.VerboseFlag {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	}
 }
