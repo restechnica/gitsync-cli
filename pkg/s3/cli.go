c0a40eb96129f78329296d524f92a203695a2240
Olivier Van Aken <olivier.va@outlook.com>
2022-11-28 01:45:20 +0100

    implemented s3 CLI implementation of the s3 interface

---
+++ pkg/s3/cli.go
@@ -0,0 +1,20 @@
+package s3
+
+import cmder "github.com/restechnica/go-cmder/pkg"
+
+// CLI a s3.API to interact with the aws s3 CLI.
+type CLI struct {
+	Commander cmder.Commander
+}
+
+// NewCLI creates a new CLI with a commander to run s3 commands.
+// Returns the new CLI.
+func NewCLI() *CLI {
+	return &CLI{Commander: cmder.NewExecCommander()}
+}
+
+// Sync syncs to and from s3.
+// Returns an error if the command fails.
+func (api CLI) Sync(source string, destination string) (output string, err error) {
+	return api.Commander.Output("aws", "s3", "sync", source, destination)
+}
