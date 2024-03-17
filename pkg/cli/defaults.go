7946e0cf7664eff52543e8a649273fe216d05029
Olivier Van Aken <olivier.va@outlook.com>
2024-03-18 00:18:14 +0100

    added local git target to default targets, as last option to find compatibility with

--- pkg/cli/defaults.go
+++ pkg/cli/defaults.go
@@ -5,7 +5,8 @@ import "github.com/restechnica/gitsync-cli/pkg/target"
 var (
 	// DefaultTargets the default available targets for sources and destinations.
 	DefaultTargets = []target.Target{
-		target.NewGitTarget(),
+		target.NewRemoteGitTarget(),
 		target.NewS3Target(),
+		target.NewLocalGitTarget(),
 	}
 )
