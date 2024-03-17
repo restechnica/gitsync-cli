cb17493a23dd8f0c31b30f29566e55f1e7987094
Olivier Van Aken <olivier.va@outlook.com>
2024-03-18 00:17:32 +0100

    added git clone logic to git api/cli

--- pkg/git/api.go
+++ pkg/git/api.go
@@ -3,6 +3,7 @@ package git
 // API interface to interact with git.
 type API interface {
 	AddConfig(key string, value string) (output string, err error)
+	Clone(repository string, path string) (output string, err error)
 	FetchAll() (output string, err error)
 	InitBareRepository(path string) (output string, err error)
 	GetStatus() (output string, err error)
